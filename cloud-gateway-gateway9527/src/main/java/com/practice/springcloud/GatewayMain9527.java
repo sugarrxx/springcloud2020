237522e84f298d11182d5fdf78582dcc5b158afd
lixiaoxiang <lixiaoxiang@vpgame.cn>
2020-03-25 00:20:41 +0800

    Gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/practice/springcloud/GatewayMain9527.java
@@ -0,0 +1,18 @@
+package com.practice.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author lixiaoxiang
+ * @date 2020-03-24
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
