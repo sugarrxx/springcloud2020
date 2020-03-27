d18186fc983662a32b00e4e312f8c8567aa18e22
lixiaoxiang <lixiaoxiang@vpgame.cn>
2020-03-28 01:03:43 +0800

    Spring Cloud Alibaba Nacos

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/practice/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.practice.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lixiaoxiang
+ * @date 2020-03-28
+ */
+@RestController
+@RefreshScope   //SpringCloud原生注解 支持Nacos的动态刷新功能
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
